135ed36f542957f9edc951532e3a4954f6b3f23a
Dabronzo <ayrtonterceiro@gmail.com>
2022-02-16 19:08:52 +0000

    Added the first view and displaying database driven objects

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostView(generic.ListView):
+    """Class view for the posts"""
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
+
+
