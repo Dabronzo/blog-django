cb7ccab3c7c3cc48101b467a890f2f1112e70879
Dabronzo <ayrtonterceiro@gmail.com>
2022-02-16 15:35:06 +0000

    Added some customization to admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Coments
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ('title', 'content')
+    summernote_fields = ('content')
+
+
+@admin.register(Coments)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
