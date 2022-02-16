47e1e28261dc00216f013203e04ac79ec32f94b2
Dabronzo <ayrtonterceiro@gmail.com>
2022-02-16 11:28:21 +0000

    Made the first two models for db, Post and Coment

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Post'))
+
+
+class Post (models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_pn = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def get_number_like(self):
+        return self.likes.count()
+
+
+
+class Coments (models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_coment')
+    approved = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering= ['created_on']
+
+    def __str__(self):
+        return f"Comment: {self.body} by {self.name}"
